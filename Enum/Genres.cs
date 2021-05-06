93e6dec24f463298c3e672cedffeaec976821168
mathesbh <mathesbh@gmail.com>
2021-05-06 16:54:40 -0300

    Adiciona enum generos

---
+++ Enum/Genres.cs
@@ -0,0 +1,18 @@
+namespace DIO.Series
+{
+    public enum Genres
+    {
+        Action = 1,
+        Adventure = 2,
+        Animation = 3,
+        Comedy = 4,
+        Documentary = 5,
+        Drama = 6,
+        Fiction = 7,
+        Horror = 8,
+        Musical = 9,
+        Romantic = 10,        
+        Thriller = 11,
+        Western = 12
+    }
+}
