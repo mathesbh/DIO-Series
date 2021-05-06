42afe769ed8bc17ddb3f3515620e4a73d67c78b5
mathesbh <mathesbh@gmail.com>
2021-05-06 16:33:29 -0300

    Adiciona classe Serie

---
+++ Class/Serie.cs
@@ -0,0 +1,9 @@
+namespace DIO.Series.Class
+{
+    public class Serie
+    {
+        private string Title { get; set; }
+        private string Description { get; set; }
+        private int Year { get; set; }
+    }
+}
