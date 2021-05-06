6c73c52ea75e0fcec87219a66169b745faf2b9aa
mathesbh <mathesbh@gmail.com>
2021-05-06 16:55:55 -0300

    Inclui enum generos

--- Class/Serie.cs
+++ Class/Serie.cs
@@ -2,6 +2,7 @@ namespace DIO.Series
 {
     public class Serie
     {
+        private Genres Genres { get; set; }
         private string Title { get; set; }
         private string Description { get; set; }
         private int Year { get; set; }
