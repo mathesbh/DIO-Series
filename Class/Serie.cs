1c138a55cac8467d7957366feac73ed6fdfa8658
mathesbh <mathesbh@gmail.com>
2021-05-06 17:01:56 -0300

    Inclui metodo construtor

--- Class/Serie.cs
+++ Class/Serie.cs
@@ -1,10 +1,18 @@
 namespace DIO.Series
 {
-    public class Serie
+    public class Serie : Entity
     {
         private Genres Genres { get; set; }
         private string Title { get; set; }
         private string Description { get; set; }
         private int Year { get; set; }
+        public Serie(int id, Genres genres, string title, string description, int year)
+        {
+            this.Id = id;
+            this.Genres = genres;
+            this.Title = title;
+            this.Description = description;
+            this.Year = year;
+        }
     }
 }
