a8b055d14f6bfff786d3591b7600ddd3b27cbbbb
mathesbh <mathesbh@gmail.com>
2021-05-10 08:47:25 -0300

    Adiciona metodo menu usuario

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace DIO.Series
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string userOption = UserMenu();
         }
+        private static string UserMenu()
+		{
+			Console.WriteLine();
+			Console.WriteLine("Select an option from the menu:");
+
+			Console.WriteLine("1- To list series");
+			Console.WriteLine("2- To insert new serie");
+			Console.WriteLine("3- To update serie");
+			Console.WriteLine("4- Delete serie");
+			Console.WriteLine("5- Show serie");
+			Console.WriteLine("C- To clean window");
+			Console.WriteLine("X- To exit");
+			Console.WriteLine();
+
+			string userOption = Console.ReadLine().ToUpper();
+			Console.WriteLine();
+			return userOption;
+		}
     }
 }
